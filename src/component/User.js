1867631d638ef0dc71857338d0821f8d202fbe75
mehmetmiracaticioglu <mehmetmiracaticioglu@gmail.com>
2021-08-02 18:53:27 +0300

    state eklendi

--- src/component/User.js
+++ src/component/User.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+
+
 class User extends Component {
+    state = { 
+        isVisiable : false
+    }
     static defaultProps={
         name:"bilgi yok",
         departman:"bilgi yok",
         salary:"bilgi yok"
 }
-constructor(props){
-    super(props);
 
-    this.state={
-        isVisiable : false
-    }
+onClickEvent=(e)=>{
+   this.setState({
+       isVisiable : !this.state.isVisiable
+   })
 }
+
+
+// constructor(props){
+//     super(props);
+
+//     this.state={
+//         isVisiable : false
+//     }
+// }
 render() {
         //destrunting
         const{name,departman,salary}=this.props;
@@ -22,7 +35,7 @@ render() {
             <div className = "col-md-8 mb-4">
                 <div className="card">
                     <div className="card-header d-flex justify-content-between">
-                        <h4 className="d-inline">{name}</h4>
+                        <h4 className="d-inline " onClick={this.onClickEvent}>{name}</h4>
                         <i className="far fa-trash-alt" style= {{cursor:"pointer"}}></i>
                     </div>
                     {isVisiable ? <div className="card-body">
